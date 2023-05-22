dc5af7db5dce6a1d9dca20d5d2473a0a2b71009a
Ian Grant <ian@iangrant.me>
2023-05-22 21:09:12 +0100

    feat: add PiKVM available update sensor

--- sensor_types/sysinfo.py
+++ sensor_types/sysinfo.py
@@ -2,6 +2,7 @@ import time, psutil
 from typing import Optional
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from pacman import get_installed as installed_packages
 from .measurementerror import MeasurementError
 
 class sysinfo():
@@ -9,6 +10,29 @@ class sysinfo():
   manufacturer = 'Raspberry Pi'
   model = 'BCMxxxx'
   timezone = 'Europe/London'
+
+  pikvm_arch_packages = [
+      'kvmd',
+      'kvmd-fan',
+      'kvmd-oled',
+      'kvmd-webterm',
+      'kvmd-cloud',
+      'kvmd-platform-v2-hdmi-rpi4',
+      'janus-gateway-pikvm',
+      'firmware-raspberrypi-pikvm',
+      'avrdude-pikvm',
+      'pikvm-os-raspberrypi',
+      'linux-rpi-pikvm',
+      'linux-rpi-headers-pikvm',
+      'linux-firmware-whence-pikvm',
+      'linux-firmware-pikvm',
+      'linux-api-headers-pikvm',
+      'raspberrypi-bootloader-pikvm',
+      'raspberrypi-bootloader-x-pikvm',
+      'raspberrypi-firmware-pikvm',
+      'ustreamer',
+      'tailscale-pikvm'
+    ]
 
   def __init__(self, addr: Optional[str] = None, config: Optional[dict] = None):
     self.model = "{bcm_model} ({rpi_model})".format(bcm_model=self.bcm_model, rpi_model=self.rpi_model)
@@ -44,6 +68,16 @@ class sysinfo():
     """The average system load over the last minute, in %."""
     try:
       return psutil.getloadavg()[0] / psutil.cpu_count() * 100
+    except Exception as error:
+      raise MeasurementError(str(error))
+
+  @property
+  def update_available(self):
+    """Whether any OS packages for PiKVM have updates available."""
+    try:
+      packages = installed_packages()
+      upgradable = list(filter(lambda p: p['id'] in self.pikvm_arch_packages and p['upgradable'] is True, packages))
+      return "ON" if len(upgradable) > 0 else "OFF"
     except Exception as error:
       raise MeasurementError(str(error))
 
