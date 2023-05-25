6d5b6293d3289a727356d70c74faca9edb83bfda
Ian Grant <ian@iangrant.me>
2023-05-25 22:04:44 +0100

    chore: adjust install script to skip some files

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git archive HEAD | tar -x -C /var/lib/kvmd/pst/data/pikvm-ha-sensors
+git archive HEAD | tar -x -C /var/lib/kvmd/pst/data/pikvm-ha-sensors \
+  --exclude='*sample*' \
+  --exclude='pikvm-ha-sensors.service' \
+  --exclude='install.sh' \
+  --exclude='requirements.txt'
 cp pikvm-ha-sensors.service /etc/systemd/system/
 systemctl daemon-reload
