2bd29b11e2da7b09be47693b5ac468d70848eb8f
Ian Grant <ian@iangrant.me>
2026-01-23 07:34:17 +0000

    chore: adds some minor excludes to the installer

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ git archive HEAD | tar -x -C /var/lib/kvmd/pst/data/pikvm-ha-sensors \
   --exclude='*sample*' \
   --exclude='pikvm-ha-sensors.service' \
   --exclude='install.sh' \
+  --exclude='*.md' \
+  --exclude='.vscode' \
 && uv sync --directory /var/lib/kvmd/pst/data/pikvm-ha-sensors
 cp pikvm-ha-sensors.service /etc/systemd/system/ && systemctl daemon-reload
