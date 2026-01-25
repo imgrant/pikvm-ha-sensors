19b535e02dc3acc542df82e361b9b807c4be9159
Ian Grant <ian@iangrant.me>
2026-01-25 21:12:11 +0000

    fix: sets the uv cache dir to be on the user data filesystem, alongside deployed program.

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 set -e
 
 INSTALL_DIR="/var/lib/kvmd/pst/data/pikvm-ha-sensors"
+export UV_WORKING_DIR="$INSTALL_DIR"
+export UV_CACHE_DIR="$INSTALL_DIR/.cache/uv"
 
 if [ ! -d "$INSTALL_DIR/.venv" ]; then
-  uv venv --system-site-packages --directory $INSTALL_DIR
+  uv venv --system-site-packages
 fi
 
 git archive HEAD | tar -x -C $INSTALL_DIR \
@@ -15,7 +17,7 @@ git archive HEAD | tar -x -C $INSTALL_DIR \
   --exclude='*.md' \
   --exclude='.vscode' \
 
-uv sync --directory $INSTALL_DIR
+uv sync
 
 cp pikvm-ha-sensors.service /etc/systemd/system/
 systemctl daemon-reload
