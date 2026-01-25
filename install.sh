d7060d3bd0ba574eb615de0489d4e1aa3ada57e1
Ian Grant <ian@iangrant.me>
2026-01-25 12:26:31 +0000

    fix: system-site-packages must be done in venv setup with uv, not pyproject.toml

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-git archive HEAD | tar -x -C /var/lib/kvmd/pst/data/pikvm-ha-sensors \
+set -e
+
+INSTALL_DIR="/var/lib/kvmd/pst/data/pikvm-ha-sensors"
+
+if [ ! -d "$INSTALL_DIR/.venv" ]; then
+  uv venv --system-site-packages --directory $INSTALL_DIR
+fi
+
+git archive HEAD | tar -x -C $INSTALL_DIR \
   --exclude='*sample*' \
   --exclude='pikvm-ha-sensors.service' \
   --exclude='install.sh' \
   --exclude='*.md' \
   --exclude='.vscode' \
-&& uv sync --directory /var/lib/kvmd/pst/data/pikvm-ha-sensors
-cp pikvm-ha-sensors.service /etc/systemd/system/ && systemctl daemon-reload
+
+uv sync --directory $INSTALL_DIR
+
+cp pikvm-ha-sensors.service /etc/systemd/system/
+systemctl daemon-reload
